50d53150d9163b79b99c051c257cb793fef1ba4f
唐卓 <tangz@yomob.com>
2019-01-09 13:31:19 +0800

    适配IPhone刘海，去掉test

---
+++ Assets/Scripts/UI/AdaptSkill.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdaptSkill : MonoBehaviour {
+	public Transform skill;
+	// Use this for initialization
+	void Start () {
+		if (UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneX ||
+		   UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXR ||
+		   UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXS ||
+		   UnityEngine.iOS.Device.generation == UnityEngine.iOS.DeviceGeneration.iPhoneXSMax) {
+			skill.localPosition += new Vector3 (70,0,0);
+		}
+	}
+	
+
+}
